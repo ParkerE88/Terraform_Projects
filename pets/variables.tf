847950d512f529c6145c763156549f078579b25f
Parker <parkevans88@gmail.com>
2022-09-15 10:37:42 -0600

    pets/variable.tf

--- pets/variables.tf
+++ pets/variables.tf
@@ -1,24 +1,19 @@
 variable "filename"{
     default = "pets/pet.txt"
-    type = string
 }
 
 variable "content"{
     default = "We love pets!"
-    type = string
 }
 
 variable "prefix"{
     default = "Mrs"
-    type = string
 }
 
 variable "separator"{
     default = "."
-    type = string
 }
 
 variable "length"{
-    default = "1"
-    type = string
+    default = 2
 }
